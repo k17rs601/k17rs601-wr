0ad80c09910a0ed0410ff8ac58e75d787331c888
Hiroki SHIIHATA <44153010+k17rs601@users.noreply.github.com>
2022-11-15 10:07:25 +0900

    何が変わったんだ‥

--- reserve/res2.php
+++ reserve/res2.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <?php
 session_start();
 echo $_COOKIE['PHPSESSID'] . "<br>";
+$conn = new mysqli("localhost", "root", "", "FAMIs"); //MySQLサーバへ接続
+$conn->set_charset('utf8'); //データベースとの通信をUTF8で行う。
+$sql = "INSERT FROM tbl_user";
+$sql1 = "SELECT MAX(resn) AS max FROM tbl_res WHERE resn < 999"; //即時予約の番号の最大値を取得
+
+if ($conn->connect_error) {
+    die($conn->connect_error);
+}
+
 ?>
 
 <head>
@@ -17,10 +27,10 @@ echo $_COOKIE['PHPSESSID'] . "<br>";
     予約が完了いたしました。<br>
     <h2>注文内容</h2><br>
     <?php
-    $uid = $_POST['uid'];
-    $pass = $_POST['pass'];
-    date_default_timezone_set('Asia/Tokyo');
-    echo date('Y-m-d H:i') . " "; //現在日付 2020-06-22
+    $howp = $_POST['howp']; //人数取得
+    echo $howp . "<br>";
+    date_default_timezone_set('Asia/Tokyo'); //基準時刻を日本に設定
+    echo date('Y-m-d H:i') . " "; //現在日付時間を表示
     ?>
     予約番号
     <button onclick="location.href='top.php'">TOPに戻る
--- reserve/top.php
+++ reserve/top.php
@@ -21,30 +21,39 @@ session_regenerate_id();
     text-align: center;
   }
 </style>
-<h1 style="text-align:center">FAMIs予約サイト</h1>
-<?php echo '<h4 style="text-align:right">ようこそ、' . $_SESSION["uname"] . 'さん<h4>' ?>
-<h3>現在の順番の待ち組数</h3>
-<div style="font-size:20px; text-align:center">
-  <input type="text" id="rescount" name="rescount" value="0" class="txt1" style="text-align:right" readonly> 組
-</div>
-<h3>お客様のご予約番号</h3>
-<div style="text-align:center">
-  <input type="text" id="rnum" name="rescount" value="" class="txt1" placeholder="現在ご予約がございません。予約を行ってください。" style="text-align:center" readonly>
-</div>
-<h3>予約はコチラから</h3>
 
-<ul style="text-align:left">
-  <li><a href="res1.php">今からご来店のご予約</a></li>
-  <li><a href="resi1.php">日付時間指定のご予約</a></li>
-  <li><a href="res3.php">Quick予約</a></li>
+<body>
   <?php
-  if (!$resn) {
-    echo '<li><button onclick="location.href=' . "'./login.html" . "'" . '" disabled>' . '予約キャンセル</li>';
-  } else {
-    echo '<li><button onclick="location.href=' . "'./login.html" . "'" . '">' . '予約キャンセル</li>';
+  if (isset($_SESSION["count1"])) {
+    die("セッションエラーです。再度ログインしてください。");
   }
   ?>
-</ul>
+  <h1 style="text-align:center">FAMIs予約サイト</h1>
+  <?php
+  echo '<h4 style="text-align:right">ようこそ、' . $_SESSION["uname"] . 'さん<h4>'
+  ?>
+  <h3>現在の順番の待ち組数</h3>
+  <div style="font-size:20px; text-align:center">
+    <input type="text" id="rescount" name="rescount" value="0" class="txt1" style="text-align:right" readonly> 組
+  </div>
+  <h3>お客様のご予約番号</h3>
+  <div style="text-align:center">
+    <input type="text" id="rnum" name="rescount" value="" class="txt1" placeholder="現在ご予約がございません。予約を行ってください。" style="text-align:center" readonly>
+  </div>
+  <h3>予約はコチラから</h3>
+
+  <ul style="text-align:left">
+    <li><a href="res1.php">今からご来店のご予約</a></li>
+    <li><a href="resi1.php">日付時間指定のご予約</a></li>
+    <li><a href="res3.php">Quick予約</a></li>
+    <?php
+    if (!$resn) {
+      echo '<li><button onclick="location.href=' . "'./login.html" . "'" . '" disabled>' . '予約キャンセル</li>';
+    } else {
+      echo '<li><button onclick="location.href=' . "'./login.html" . "'" . '">' . '予約キャンセル</li>';
+    }
+    ?>
+  </ul>
 
 </body>
 
