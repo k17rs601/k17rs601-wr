0ae51d570df5de03943b208a61d0fd21f47f1c81
Hiroki SHIIHATA <44153010+k17rs601@users.noreply.github.com>
2022-11-22 10:16:15 +0900

    データの値を修正

--- reserve/res2.php
+++ reserve/res2.php
@@ -9,10 +9,13 @@ if (isset($_SESSION["count1"])) {
 
 
 <?php
-
+date_default_timezone_set('Asia/Tokyo');     //基準時刻を日本に設定
+$date = date('Y-m-d');
+echo $date;
 $conn = new mysqli("localhost", "root", "", "FAMIs"); //MySQLサーバへ接続
 $conn->set_charset('utf8'); //データベースとの通信をUTF8で行う。
-$sql1 = "SELECT MAX(reserve_number) FROM tbl_res WHERE reserve_number < 999 and DATE(res_datetime) = '2022-11-17';"; //即時予約の番号の最大値を取得
+//$sqlid = "SELECT MAX(reserve_number) FROM tbl_res WHERE reserve_number < 999 and DATE(res_datetime) = '';";
+$sql1 = "SELECT MAX(reserve_number) FROM tbl_res WHERE reserve_number < 999 and DATE(res_datetime) = '$date' ;"; //即時予約の番号の最大値を取得
 $PeopleNumber = $_POST['howp']; //人数取得
 
 $rs = $conn->query($sql1);
@@ -25,6 +28,8 @@ if ($row["MAX(reserve_number)"] < 100) {
 } else {
     $res_number = $row["MAX(reserve_number)"] + 1; //新しい予約番号
 }
+
+
 if ($PeopleNumber < 6) { //必要なテーブル数の計算
     $TableNumber = 1;
 } else {
@@ -52,8 +57,6 @@ $rs = $conn->query($sql);
     予約が完了いたしました。<br>
     <h2>注文内容</h2><br>
     <?php
-
-    date_default_timezone_set('Asia/Tokyo'); //基準時刻を日本に設定
     echo date('Y-m-d H:i') . " 予約番号" . $res_number; //現在日付時間を表示
     ?>
     <button onclick="location.href='top.php'">TOPに戻る
